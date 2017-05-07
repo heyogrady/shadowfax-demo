070208f50c5abeb98c7eeb3e3c81fa9df7d761c5
Patrick O'Grady <ogradypatrickj@gmail.com>
2017-05-07 12:42:54 -0400

    Add modal behavior javascript

--- app/assets/javascripts/common/semantic-ui.js
+++ app/assets/javascripts/common/semantic-ui.js
@@ -1,4 +1,4 @@
-document.addEventListener("turbolinks:load", function(){
+document.addEventListener("turbolinks:load", function() {
   // Initialize Semantic UI dropdowns
   $("[data-ui-behavior=dropdown]").dropdown();
 
