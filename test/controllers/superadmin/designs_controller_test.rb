c779e87bba933e9596cdef29e727e64b4e76448c
Patrick O'Grady <ogradypatrickj@gmail.com>
2017-06-02 12:56:40 -0400

    #11 - Add page templates

--- test/controllers/superadmin/designs_controller_test.rb
+++ test/controllers/superadmin/designs_controller_test.rb
@@ -7,8 +7,38 @@ class Superadmin::DesignsControllerTest < ActionController::TestCase
     sign_in(user)
   end
 
+  def test_designs_templates_index
+    get :templates_index
+    assert_response :success
+  end
+
   def test_designs_templates_show
     get :templates_show
+    assert_response :success
+  end
+
+  def test_designs_templates_new
+    get :templates_new
+    assert_response :success
+  end
+
+  def test_designs_templates_dashboard
+    get :templates_dashboard
+    assert_response :success
+  end
+
+  def test_designs_templates_wizard
+    get :templates_wizard
+    assert_response :success
+  end
+
+  def test_designs_templates_settings
+    get :templates_settings
+    assert_response :success
+  end
+
+  def test_designs_templates_components
+    get :templates_components
     assert_response :success
   end
 
